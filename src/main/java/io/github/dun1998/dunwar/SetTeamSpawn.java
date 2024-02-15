d3aad2953580546a7c32858071697f8b67925853
DESKTOP-4EJLGAO\Duncan <duncanmacmurchy@gmail.com>
2024-02-14 16:27:16 -0800

    Added the ability to set a team's spawn point.

--- src/main/java/io/github/dun1998/dunwar/SetTeamSpawn.java
+++ src/main/java/io/github/dun1998/dunwar/SetTeamSpawn.java
@@ -1,4 +1,42 @@
 package io.github.dun1998.dunwar;
 
-public class SetTeamSpawn {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class SetTeamSpawn implements CommandExecutor {
+
+    DunWar plugin;
+    Game game;
+    public SetTeamSpawn(DunWar plugin,Game game){
+        this.plugin = plugin;
+        this.game = game;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if(sender instanceof Player){
+            if(args.length>0){
+                String teamName = args[0];
+                for(WarTeam team:game.warTeams){
+                    if(team.name.equalsIgnoreCase(teamName)){
+                        if(team.base==null){
+                            sender.sendMessage("Please set a team base first.");
+                            return true;
+                        }
+                        team.base.SetTeamSpawn(sender);
+                        return true;
+                    }
+                }
+                sender.sendMessage(String.format("Failed to locate a team with the name %s",teamName));
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
 }
